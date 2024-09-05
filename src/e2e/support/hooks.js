f6722600cf2bff777425a26fc49740d38a8c2d4e
Seema Semwal <ssemwal@adobe.com>
2024-09-05 16:39:59 +0530

    Prod event handling logic in before hook

--- src/e2e/support/hooks.js
+++ src/e2e/support/hooks.js
@@ -6,24 +6,30 @@ const logger = new Logger();
 let isScenarioExecuted = false;
 
 Before(async function (scenario) {
+  const env = process.env.ENV || null
   this.credentials = {
     username: process.env.USERNAME || testData.userInfo.username,
     password: process.env.PASSWORD || testData.userInfo.password,
   };
 
-  const scenarioName = scenario.pickle.name; 
+  const scenarioName = scenario.pickle.name;
   logger.logHeading(`Starting Scenario: ${scenarioName}`);
 
   if (scenarioName === 'Validate and register events') {
     this.overrideEventName = process.env.EVENT || null;
+    if (env && env === 'prod' && !this.overrideEventName) {
+      logger.logError("Skipping scenario: EVENT not provided for production");
+      this.skip();
+    }
     if (this.overrideEventName && isScenarioExecuted) {
-      return 'skipped';
+      console.log("Skipping scenario: Event already executed.");
+      this.skip();
     }
   }
 });
 
 After(function (scenario) {
-  const scenarioName = scenario.pickle.name; 
+  const scenarioName = scenario.pickle.name;
   if (scenarioName === 'Validate and register events') {
     isScenarioExecuted = true;
   }
